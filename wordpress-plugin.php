38cc8dd42046cb36601b49ce2575fb435b859f3b
JoelCDL <jhagedorn@ucop.edu>
2026-07-31 17:06:02 -0700

    Add plugin version to script and stylesheet
    Automatically append the script and stylesheet with the plugin version to prevent older cached versions of those files from loading in browsers.

--- wordpress-plugin.php
+++ wordpress-plugin.php
@@ -7,10 +7,17 @@ Version:      1.3.0
 */
 
 function add_klaro_ui_stylesheet_and_script() {
-  $plugin_url = plugin_dir_url( __FILE__ );
+    $plugin_path = plugin_dir_path( __FILE__ ) . 'wordpress-plugin.php';
 
-  wp_enqueue_style( 'klaro-ui', $plugin_url . 'dist/css/klaro-ui.css' );
-  wp_enqueue_script( 'klaro-config', $plugin_url . 'dist/js/klaro-config.js' );
+    if ( ! function_exists( 'get_plugin_data' ) ) {
+        require_once ABSPATH . 'wp-admin/includes/plugin.php';
+    }
+
+    $plugin_data = get_plugin_data( $plugin_path );
+    $plugin_version = $plugin_data['Version'];
+
+  wp_enqueue_style( 'klaro-ui', plugins_url( 'dist/css/klaro-ui.css', __FILE__ ), array(), $plugin_version );
+  wp_enqueue_script( 'klaro-config', plugins_url( 'dist/js/klaro-config.js', __FILE__ ), array(), $plugin_version, true );
 }
 
 add_action( 'wp_enqueue_scripts', 'add_klaro_ui_stylesheet_and_script' );
